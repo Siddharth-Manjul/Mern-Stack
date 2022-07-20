9388f5300929592878f462837e07a91d894231ee
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-07-20 21:08:40 +0530

    5th commit

---
+++ Reduce/main.js
@@ -0,0 +1,40 @@
+/* Reduce - 
+iterates, callback function
+reduces to a single value - number, array , object.
+1st parameter ('acc') - total of all calculations. 'acc' stands for "accumulator".
+2nd parameter ('curr') - current iteration/value. */
+
+const staff = [
+  {
+    name: "bob",
+    age: 20,
+    position: "developer",
+    salary: 100,
+  },
+  {
+    name: "peter",
+    age: 25,
+    position: "designer",
+    salary: 300,
+  },
+  {
+    name: "susy",
+    age: 30,
+    position: "the boss",
+    salary: 400, 
+  },
+  {
+    name: "anna",
+    age: 35,
+    position: "intern",
+    salary: 10,
+  },
+];
+
+const dailySalary = staff.reduce((total, person) => {
+  total += person.salary;
+
+  return total
+},0)
+
+console.log(dailySalary);

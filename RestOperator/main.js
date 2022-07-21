b42eb92dc88944fbb5cc5769d0f3d73ea16b7818
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-07-21 22:03:22 +0530

    9th commit

---
+++ RestOperator/main.js
@@ -0,0 +1,16 @@
+// Rest Operator... - 
+// Gathers/Collects Items.
+// Destructuring, Functions.
+// Placement important, careful with the same name.
+// Rest when declare Function, spread when invoke the Function.
+
+// Arrays.
+const fruits = ['apple', 'orange', 'lemon', 'banana'];
+const [first] = fruits;
+
+// Objects.
+const person = {name: 'John', lastName: 'Smith', job: 'Developer'};
+const {name} = person;
+console.log(name);
+
+// Functions.

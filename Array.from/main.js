437f5ab6e47ed3a34a065f59d70011414eb00e6d
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-07 00:02:13 +0530

    14th commit

--- Array.from/main.js
+++ Array.from/main.js
@@ -5,3 +5,24 @@
 // from - turns array-like into array - string, nodeList, Set.
 
 const udemy = 'udemy';
+// console.log(Array.from(udemy));
+
+const text = document.querySelectorAll(".text");
+
+const newText = Array.from(text).find((item) => item.textContent === 'person');
+// console.log(newText);
+
+// Pagination functionality. 
+const items = Array.from({length: 120}, (_, index) => {
+  return index
+})
+
+const itemsPerPage = 14;
+const pages = Math.ceil(items.length / itemsPerPage);
+
+const newItems = Array.from({length: pages}, (_, index) => {
+  const start = index * itemsPerPage
+  const tempItems = items.slice(start, start + itemsPerPage)
+  return tempItems
+})
+console.log(newItems);

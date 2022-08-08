5468f2a4191e5710765305f6183a2c224b362a6e
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-08 23:47:19 +0530

    17th commit

--- CallbackFunction/main.js
+++ CallbackFunction/main.js
@@ -4,12 +4,26 @@ function makeUppercase(value) {
 
 // makeUppercase('peter');
 
-function handleName(name, cb) {
-  const fullName = `${name} smith`
-  cb(fullName)
+function reverseString(value) {
+  console.log(value.split("").reverse().join(""));
+}
+
+function handleName(name, cb, cc) {
+  const fullName = `${name} smith`;
+  cb(fullName);
   // Some more logic here.
 }
 
-handleName('peter', makeUppercase);
+handleName("peter", makeUppercase); // You must not invoke the function here "makeUppercase()" otherwise it will through an error.
+handleName("peter", reverseString);
+
+handleName("susan", (value) => console.log(value));
 
 // Array methods, setTimeout, event listener etc.
+// Wrapping and usage of Callback function in HTML.
+/*
+ const btn = document.querySelector('.btn)
+btn.addEventListener('click', function () {
+  console.log('Hello World);
+})
+*/

348c6ac2d2a9b5c420ce03f2d01d2a941ce60870
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-10 23:46:27 +0530

    20th commit

--- Promises/main.js
+++ Promises/main.js
@@ -9,10 +9,10 @@ const value = 2;
 
 const promise = new Promise((resolve, reject) => {
   const random = Math.floor(Math.random() * 3);
-  if(random === value) {
-    resolve('You guessed correctly');
+  if (random === value) {
+    resolve("You guessed correctly");
   } else {
-    reject('Wrong Number');
+    reject("Wrong Number");
   }
 });
 
@@ -20,3 +20,28 @@ console.log(promise);
 
 // promise.then((data) => console.log(data));
 promise.catch((err) => console.log(err));
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  addColor(1000, ".first", "red")
+  .then(() => addColor(3000, ".second", "blue"))
+  .then(() => addColor(2000, ".third", "green"))
+  .catch((err) =>
+    console.log(err)
+  );
+});
+
+function addColor(time, selector, color) {
+  const element = document.querySelector(selector);
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(`There is no such element: "${selector}"`);
+    }
+  });
+}

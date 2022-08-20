1d0442e8bd86ec449786ffa6a486087e02d60881
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-20 22:36:32 +0530

    27th commit

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,1 +1,11 @@
-// Modules.
+// CommonJS, every file in node is a module (by default).
+// Modules - Encapsulated code (only sharing minimum or 'what we want').
+
+const names = require('./names');
+const sayHi = require('./utils');
+
+sayHi('susan'); 
+sayHi(names.john);
+sayHi(names.peter);
+
+
---
+++ NodeJS/names.js
@@ -0,0 +1,7 @@
+// Not sharing.
+const secret = 'SUPER SECRET';
+// Sharing.
+const john = 'john';
+const peter = 'peter';
+
+module.exports = {john, peter};

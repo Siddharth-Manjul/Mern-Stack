e0083603b97fd05a0acf0695c6e1822261cd98ae
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-24 23:44:53 +0530

    34th commit

---
+++ NodeJS/npm.js
@@ -0,0 +1,9 @@
+//  npm - global command, comes with node.
+// npm --version
+
+// Local dependency - Use it only in this particular project.
+// npm i <packageName>
+
+// Global dependency - use it in any project.
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)

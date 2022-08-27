ed9c5b7514a16180976b43b920bd254cb4da7553
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-08-27 23:43:17 +0530

    41st commit

---
+++ NodeJS/EventLoop/setInterval.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+  console.log("Hello World!");
+}, 2000);
+
+console.log(`I will run first`);
+
+// PRocess stays alive unless 
+// Kill PRocess CONTROL + C
+// Unexpected error

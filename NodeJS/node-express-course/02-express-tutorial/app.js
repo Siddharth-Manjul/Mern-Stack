2b687c639a4406a962c916dde71b0b58ac2929df
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-09-08 20:58:09 +0530

    Server setup with Http Module

--- NodeJS/node-express-course/02-express-tutorial/app.js
+++ NodeJS/node-express-course/02-express-tutorial/app.js
@@ -2,21 +2,45 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 // Get all files
-const homePage = readFileSync("./basic-node.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogo = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   // console.log(req);
   const url = req.url;
+  // Home Page
   if (url === "/") {
     res.writeHead(200, { content: "text/html" });
     res.write(homePage);
     res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { content: "text/html" });
+  }
+  // About Page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>About Page</h1>");
     res.end();
+  }
+  // Styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // Image/Logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeLogo);
+    res.end();
+  }
+  // JS
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
   } else {
-    res.writeHead(404, { content: "text/html" });
+    res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1>Page not found</h1>");
     res.end();
   }

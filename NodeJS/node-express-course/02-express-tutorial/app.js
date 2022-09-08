ad215eaab6d571e48596f7dd1c791cf080177b5e
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-09-08 10:56:10 +0530

    49th commit

--- NodeJS/node-express-course/02-express-tutorial/app.js
+++ NodeJS/node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const http = require("http");
+const { readFileSync } = require("fs");
+
+// Get all files
+const homePage = readFileSync("./basic-node.html");
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { content: "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { content: "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { content: "text/html" });
+    res.write("<h1>Page not found</h1>");
+    res.end();
+  }
+  console.log("User just hit the server");
+});
+
+server.listen(5000);

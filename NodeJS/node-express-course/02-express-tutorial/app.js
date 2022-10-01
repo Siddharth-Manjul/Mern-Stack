94ac2040243b0cc37d511cd51d8a6d20d10c0c01
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-10-02 00:17:56 +0530

    Methods Post(Form Example)

--- NodeJS/node-express-course/02-express-tutorial/app.js
+++ NodeJS/node-express-course/02-express-tutorial/app.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+// Static Assets
+app.use(express.static("./methods-public"));
+
+// Parse Form Data
+app.use(express.urlencoded({ extended: false }));
+
+// Read Data
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+// Insert Data
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send(`Please provide valid credentials`);
 });
 
 app.listen(5000, () => {

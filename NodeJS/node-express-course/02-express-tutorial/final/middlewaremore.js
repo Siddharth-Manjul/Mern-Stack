611154636f1fbccba6b33b7b6c474ae0b2529720
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-10-01 23:40:46 +0530

    Methods Start

---
+++ NodeJS/node-express-course/02-express-tutorial/final/middlewaremore.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const logger = require("./logger");
+const authorize = require("./authorize");
+// req(request) => middleware => res(response)
+// app.use([logger, authorize]);
+app.use(morgan('tiny'));
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
